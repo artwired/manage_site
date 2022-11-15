5efd2548d9309d24f4c7ae3312914bc47f475f4e
Brad Cole <artwired@users.noreply.github.com>
2022-11-14 22:28:20 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const primaryHeader = document.querySelector(".primary-header")
 const navToggle = document.querySelector(".mobile-nav-toggle")
 const primaryNav = document.querySelector(".primary-navigation")
 
@@ -6,4 +7,10 @@ navToggle.addEventListener("click", () => {
         ? navToggle.setAttribute("aria-expanded", false)
         : navToggle.setAttribute("aria-expanded", true)
     primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-overlay")
 })
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: true,
+    dots: false
+  })
